074abca58b5989dadaca8d08dd3ae531d78d36f7
Joe Izang <joeizang@hotmail.com>
2019-10-14 12:37:55 +0100

    add editorconfig for every project. flesh out ApplicationUser and Error.cshtml.cs

--- blogapi/blogapi/DomainModels/ApplicationUser.cs
+++ blogapi/blogapi/DomainModels/ApplicationUser.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace blogapi.DomainModels
 {
-    public class ApplicationUser : IdentityUser
-    {
-    }
+  public class ApplicationUser : IdentityUser
+  {
+    public string FullName { get; set; }
+
+    public string NickName { get; set; }
+
+    public string TwitterHandle { get; set; }
+
+    public string GitHubHandle { get; set; }
+
+    public string EmailAddress { get; set; }
+
+    public string Facebook { get; set; }
+
+    public DateTimeOffset DateOfBirth { get; set; }
+  }
 }
